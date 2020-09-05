7a39b50ef82cfc6d7ef7c117ff8ba5db8bf266e0
María Mercedes Retolaza <ret16339@uvg.edu.gt>
2020-09-04 20:11:22 -0600

    cambios en nombres

--- frontend/src/components/RolesRelationshipFeature/rolestable/index.js
+++ frontend/src/components/RolesRelationshipFeature/rolestable/index.js
@@ -1,6 +1,6 @@
-import React, { forwardRef, useEffect } from 'react';
-import MaterialTable from 'material-table';
-import { connect } from 'react-redux';
+import React, { forwardRef, useEffect } from './node_modules/react';
+import MaterialTable from './node_modules/material-table';
+import { connect } from './node_modules/react-redux';
 import {
     getRoles
 } from '../../../reducers';
@@ -8,22 +8,22 @@ import * as actions from '../../../actions/rolesRelationship';
 import * as modalRoles from '../../../actions/modalRoles';
 import * as actionsRol from '../../../actions/selectedRol';
 
-import AddBox from '@material-ui/icons/AddBox';
-import ArrowDownward from '@material-ui/icons/ArrowDownward';
-import Check from '@material-ui/icons/Check';
-import ChevronLeft from '@material-ui/icons/ChevronLeft';
-import ChevronRight from '@material-ui/icons/ChevronRight';
-import Clear from '@material-ui/icons/Clear';
-import DeleteOutline from '@material-ui/icons/DeleteOutline';
-import Edit from '@material-ui/icons/Edit';
-import FilterList from '@material-ui/icons/FilterList';
-import FirstPage from '@material-ui/icons/FirstPage';
-import LastPage from '@material-ui/icons/LastPage';
-import Remove from '@material-ui/icons/Remove';
-import SaveAlt from '@material-ui/icons/SaveAlt';
-import Search from '@material-ui/icons/Search';
-import ViewColumn from '@material-ui/icons/ViewColumn';
-import FindInPageIcon from '@material-ui/icons/FindInPage';
+import AddBox from './node_modules/@material-ui/icons/AddBox';
+import ArrowDownward from './node_modules/@material-ui/icons/ArrowDownward';
+import Check from './node_modules/@material-ui/icons/Check';
+import ChevronLeft from './node_modules/@material-ui/icons/ChevronLeft';
+import ChevronRight from './node_modules/@material-ui/icons/ChevronRight';
+import Clear from './node_modules/@material-ui/icons/Clear';
+import DeleteOutline from './node_modules/@material-ui/icons/DeleteOutline';
+import Edit from './node_modules/@material-ui/icons/Edit';
+import FilterList from './node_modules/@material-ui/icons/FilterList';
+import FirstPage from './node_modules/@material-ui/icons/FirstPage';
+import LastPage from './node_modules/@material-ui/icons/LastPage';
+import Remove from './node_modules/@material-ui/icons/Remove';
+import SaveAlt from './node_modules/@material-ui/icons/SaveAlt';
+import Search from './node_modules/@material-ui/icons/Search';
+import ViewColumn from './node_modules/@material-ui/icons/ViewColumn';
+import FindInPageIcon from './node_modules/@material-ui/icons/FindInPage';
 
 const icons = {
     Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
