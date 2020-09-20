2d488dd6885e626c0e076288dd166366dd94ba59
DianaXimena <dianaxime0@gmail.com>
2020-09-20 00:17:23 -0600

    see more ac

--- frontend/src/components/AssociationClubFeature/SelectedAssociationClub/index.js
+++ frontend/src/components/AssociationClubFeature/SelectedAssociationClub/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { MDBCard, MDBCardBody, MDBCardImage, MDBRow, MDBCol } from
-'mdbreact';
+import { MDBCard, MDBCardBody, MDBCardImage, MDBRow, MDBCol, MDBIcon, MDBCardText } from
+    'mdbreact';
 import {
     getAuthToken,
     getAssociationClub,
@@ -9,35 +9,51 @@ import {
 } from '../../../reducers';
 import './styles.css';
 import moment from 'moment';
+import { Link } from "react-router-dom";
 
 let SelectedAssociationClub = ({
     name, startdate, enddate, type, description,
-    }) => {
+}) => {
     return (
-        <div className="dataWorkshop">
-            <MDBRow>
-                <MDBCol col='4'>
-                    <MDBCard className="cartaT">
-                    <MDBCardImage
-                        className="tituloWorkshop"
-                        tag='div'
-                    >
-                        <h2>{name}</h2>
-                    </MDBCardImage>
-                    <MDBCardBody cascade className='text-center'>
-                        <h5 className="descr">{description}</h5>
-                        <h5 className="sdatetitle">Inicio</h5>   
-                        <h5 className="sdate">{moment(startdate).format('L')}</h5>
-                        <h5 className="edatetitle">Fin</h5>
-                        <h5 className="edate">{moment(enddate).format('L')}</h5>
-                        <hr />
-                        <div className='text-center'>
-                        {type}
+        <div className="dataAC">
+            <MDBCard wide style={{ width: "22rem" }}>
+                <MDBCardImage
+                    className='view view-cascade gradient-card-header heavy-rain-gradient'
+                    cascade
+                    tag='div'
+                >
+                    <h2 className='h2-responsive mb-2' style={{ padding: '1rem' }}>{name}</h2>
+                    <MDBRow>
+                        <MDBCol>
+                            <p>
+                                <MDBIcon far icon='calendar' />  {moment(startdate).format('L')}
+                            </p>
+                        </MDBCol>
+                        <MDBCol>
+                            <p>
+                                <MDBIcon far icon='calendar' />  {moment(enddate).format('L')}
+                            </p>
+                        </MDBCol>
+                    </MDBRow>
+                </MDBCardImage>
+                <MDBRow start>
+                    <MDBCol md='0'>
+                        <Link to="/asociacionesClubs">
+                            <button className="kc_fab_main_btn">
+                                <MDBIcon icon='chevron-left' />
+                            </button>
+                        </Link>
+                    </MDBCol>
+                    <MDBCol md='12'>
+                        <div className='text-center' style={{ padding: '1rem' }}>
+                            {type}
                         </div>
-                    </MDBCardBody>
-                    </MDBCard>
-                </MDBCol>
-            </MDBRow>
+                    </MDBCol>
+                </MDBRow>
+                <MDBCardBody cascade className='text-center'>
+                    <MDBCardText>{description}</MDBCardText>
+                </MDBCardBody>
+            </MDBCard>
         </div>
     );
 }
