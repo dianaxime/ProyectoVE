2921f626f539587a1d1211b11a7533131fca4151
18250MariaInes <maria.ines.vf@gmail.com>
2020-10-03 19:48:13 -0600

    statistics done for sprint 5

---
+++ backend/src/routes/statisticsRoute.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const verifyAuth = require('../middleware/verifyAuth');
+
+const {
+    getAssistanceOfClub,
+    getAssistanceOfClubs,
+    getPlayersInTournament,
+    getPlayersOfSportInTournament,
+    getTeamsOfTournament,
+    getTeamsOfASportInTournament,
+    getGenderOfTournament,
+    getGenderOfSportInTournament,
+    getAllScholars,
+    getParticipactionWorkshopsInTime,
+    getParticipactionWorkshop,
+    getGenderParticipactionWorkshopsInTime,
+    getGenderParticipactionOfWorkshop
+} = require('../controllers/statisticsController');
+
+const router = express.Router();
+
+// statistics Routes
+
+router.get('/statistics/assistances-of-club/:idc/:startdate/:enddate', verifyAuth, getAssistanceOfClub);
+router.get('/statistics/assistances-of-clubs/:startdate/:enddate', verifyAuth, getAssistanceOfClubs);
+router.get('/statistics/players-tournament/:startdate/:enddate', verifyAuth, getPlayersInTournament);
+router.get('/statistics/players-of-sport-tournament/:startdate/:enddate/:sport', verifyAuth, getPlayersOfSportInTournament);
+router.get('/statistics/teams-tournament/:startdate/:enddate', verifyAuth, getTeamsOfTournament);
+router.get('/statistics/teams-of-sport-tournament/:startdate/:enddate/:sport', verifyAuth, getTeamsOfASportInTournament);
+router.get('/statistics/genders-tournament/:startdate/:enddate', verifyAuth, getGenderOfTournament);
+router.get('/statistics/genders-of-sport-tournament/:startdate/:enddate/:sport', verifyAuth, getGenderOfSportInTournament);
+router.get('/statistics/quantity-scholars/', verifyAuth, getAllScholars);
+router.get('/statistics/participation-workshops/:startdate/:enddate', verifyAuth, getParticipactionWorkshopsInTime);
+router.get('/statistics/participation-workshop/:idw', verifyAuth, getParticipactionWorkshop);
+router.get('/statistics/gender-participation-workshops/:startdate/:enddate', verifyAuth, getGenderParticipactionWorkshopsInTime);
+router.get('/statistics/gender-participation-workshop/:idw', verifyAuth, getGenderParticipactionOfWorkshop);
+
+module.exports = router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -16,7 +16,7 @@ const ACRoute = require('./routes/association_clubRoute');
 const ACParticipationRoute = require('./routes/association_clubRelationshipRoute');
 const sessionRoute = require('./routes/sessionsRoute');
 const assistanceRoute = require('./routes/assistanceRoute');
-
+const statisticsRoute = require('./routes/statisticsRoute');
 
 
 const app = express();
@@ -42,6 +42,8 @@ app.use('/api/v1', ACRoute);
 app.use('/api/v1', ACParticipationRoute );
 app.use('/api/v1', sessionRoute );
 app.use('/api/v1', assistanceRoute );
+app.use('/api/v1', statisticsRoute );
+//app.use('/api/v1', statisticsRoute );
 
 app.listen(env.port).on('listening', () => {
     console.log(`are live on ${env.port}`);
