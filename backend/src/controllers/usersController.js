d7e75495ab58c7b659725c22a351caddd66859c4
DianaXimena <dianaxime0@gmail.com>
2020-11-01 11:41:07 -0600

    format roles

--- backend/src/controllers/usersController.js
+++ backend/src/controllers/usersController.js
@@ -715,16 +715,23 @@ const getRole = async (req, res) => {
             errorMessage.error = 'no user found';
             return res.status(status.notfound).send(errorMessage);
         }
-    
-        successMessage.data = data;
+        let seen = {};
+        for (let i = 0; i < data.length; i++) {
+            let title = data[i].role.replace(/ /g, '')
+            seen[title] = true
+        }
+           
+        successMessage.data = seen;
         return res.status(status.success).send(successMessage);
     })
-    .catch(error => {
-        console.log('ERROR:', error); // print the error;
-        errorMessage.error = 'Operation was not successful';
-        return res.status(status.error).send(errorMessage);
-    })
-};
+        .catch(error => {
+            console.log('ERROR:', error); // print the error;
+            errorMessage.error = 'Operation was not successful';
+            return res.status(status.error).send(errorMessage);
+        })
+    };
+    
+var merge = function() { var destination = {}, sources = [].slice.call( arguments, 0 ); sources.forEach(function( source ) { var prop; for ( prop in source ) { if ( prop in destination && Array.isArray( destination[ prop ] ) ) {  destination[ prop ] = destination[ prop ].concat( source[ prop ] ); } else if ( prop in destination && typeof destination[ prop ] === "object" ) { destination[ prop ] = merge( destination[ prop ], source[ prop ] ); } else {  destination[ prop ] = source[ prop ]; } } }); return destination; }; 
 
 module.exports = {
     createUser,
